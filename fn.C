236f4c9701133dfcf5793c3baf024b71107d4426
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-12 04:22:43 -0700

    tttt.C
    good

---
+++ fn.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int student
+{
+    char name[50];
+    int roll;
+};
+
+void display(struct student stu);
+int main()
+{
+    struct student stud;
+    printf("Enter student's name: ");
+    scanf("%s", &stud.name);
+    printf("Enter roll number:");
+    scanf("%d", &stud.roll);
+    display(stud);
+    return 0;
+}
+void display(struct student stu){
+  printf("Output\nName: %s",stu.name);
+  printf("\nRoll: %d",stu.roll);
+}
